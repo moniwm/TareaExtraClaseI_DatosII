2603015071c4e6acbc3865dae8bae777b3de432f
Monica Waterhouse <mwaterhouse@estudiantec.cr>
2020-09-21 22:35:36 -0600

    Create client socket class

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -15,7 +15,7 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
@@ -23,18 +23,13 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
     int n;
 
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
-
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0)
         error("ERROR opening socket");
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
+    portno = 3000;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
